721139e8a617c15100f43305e10db725e6378e89
ANUBHAVNATANI <anubhavnatani99@gmail.com>
2020-04-27 19:31:11 +0530

    Updated requestis

---
+++ key.py
@@ -0,0 +1,7 @@
+#Azure credentials
+subscription_key = "674d6648b56249649069b64cc83ed9a2"
+face_api_url = 'https://relice.cognitiveservices.azure.com/face/v1.0/detect'
+face_api_url_verify = 'https://relice.cognitiveservices.azure.com/face/v1.0/verify'
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+db_key = 'dfewfew123213rwdsgert34tgfd1234trgf'
+db_uri = 'sqlite:///child.db'
